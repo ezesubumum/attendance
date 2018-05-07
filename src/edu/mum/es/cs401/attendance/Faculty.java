fc2d278fd4ca9db8392b8544f575bac5f1503633
samiuljahan <samiuljahan@gmail.com>
2018-05-07 17:14:41 -0500

    Create Class Code for Course.java and Faculty.java

--- src/edu/mum/es/cs401/attendance/Faculty.java
+++ src/edu/mum/es/cs401/attendance/Faculty.java
@@ -1,6 +1,29 @@
 package edu.mum.es.cs401.attendance;
 
 public class Faculty {
-	String ID;
-	String name;
+	private String ID;
+	private String name;
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Faculty(String ID, String name) {		
+		this.ID = ID;
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Faculty [ID=" + ID + ", name=" + name + "]";
+	}
+	
+	
 }
